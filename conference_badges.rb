8bee56a0eeec82f9f560c212594d6a2f68278dee
Zach  Smith <zdsmith001@gmail.com>
2019-07-14 17:50:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,12 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator
-  
+def batch_badge_creator(attendees)
+  attendee_list = []
+  attendees.each do |attendee|
+    attendee_list << return "Hello, my name is #{attendee}."
+  end
+  attendee_list
 end
 
 def assign_rooms
